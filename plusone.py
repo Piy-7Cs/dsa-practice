a2f3f82a6d19f7952f950e44db44e3069cde6557
redCat <user1299@duck.com>
2026-02-07 01:13:58 +0530

    plusone solved

--- plusone.py
+++ plusone.py
@@ -1,13 +1,27 @@
-digits = [9, 9, 9, 9, 9, 9, 9]
+#Leetcode 66, 
+#Time Complexity O(n)
 
-for i in range(len(digits)-1 , 0 , -1):
-    if digits[i] == 9:
-        digits[i] = 0
-        if digits[i-1] != 9 :
-            digits[i-1] = digits[i-1]+ 1
+
+digits = [0, 0]
+
+last_index = len(digits)-1
+
+
+if digits[-1] == 9:
+    for i in range(last_index, -1, -1):
+        if digits[i] == 9:
+            digits[i] = 0
+
+        if digits[i] != 0 and digits[i+1] == 0:
+            digits[i] += 1
+            break
         
+        if digits[0] == 0:
+            digits.insert(0, 1)
+
+elif digits[-1] != 9:
+    digits[-1]+=1
+
+
 
 print(digits)
-
-
-    
